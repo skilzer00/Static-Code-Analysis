fb7fb7eb54237266d5fef7e02e372940681f4059
Saadman Sakib <46536533+skilzer00@users.noreply.github.com>
2022-01-12 16:29:45 +0600

    Add files via upload

---
+++ sample2.py
@@ -0,0 +1,10 @@
+" Pylint Scoring Example"
+
+def add(number1, number2):
+    "A function to add two numbers"
+    return number1 + number2
+
+NUM_1 = 4
+NUM_2 = 5
+TOTAL = add(NUM_1, NUM_2)
+print("The sum of {NUM_1} and {NUM_2} is {TOTAL}")
